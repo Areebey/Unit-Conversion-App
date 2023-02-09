c32db3c342bc1cb26f61eae2eb690a7f58ff1c13
Areebey <areebrafiq125@gmail.com>
2023-02-10 01:52:10 +0500

    change

--- script.js
+++ script.js
@@ -9,6 +9,56 @@ let inputEl=document.getElementById("field")
 
 convertEl.addEventListener("click",function(){
     console.log("Button click")
-
-    inputEl.textContent = "22";
+inputEl=document.getElementById("field").value
+    inputEl.textContent = 22;
 })
+
+
+// ----------------------------------------------------------
+(() => {
+    let inputText = document.getElementById('text');
+    let submitBtn = document.getElementById('submit');
+    let triggerJSBtn = document.getElementById('button');
+
+    submitBtn.addEventListener('click', (event) => {
+        event.preventDefault(); // just prevent form submitted.
+    });
+
+    triggerJSBtn.addEventListener('click', (event) => {
+        const thisTarget = event.target;
+        event.preventDefault();
+        inputText.value = thisTarget.innerText;
+        inputText.dispatchEvent(new Event('input'));
+        inputText.dispatchEvent(new Event('change'));
+    });
+
+    inputText.addEventListener('beforeinput', (event) => {
+        const thisTarget = event.target;
+        console.log('beforeinput event. (%s)', thisTarget.value);
+    });
+
+    inputText.addEventListener('input', (event) => {
+        const thisTarget = event.target;
+        console.log('input event. (%s)', thisTarget.value);
+    });
+
+    inputText.addEventListener('change', (event) => {
+        const thisTarget = event.target;
+        console.log('change event. (%s)', thisTarget.value);
+    });
+
+    inputText.addEventListener('cut', (event) => {
+        const thisTarget = event.target;
+        console.log('cut event. (%s)', thisTarget.value);
+    });
+
+    inputText.addEventListener('copy', (event) => {
+        const thisTarget = event.target;
+        console.log('copy event. (%s)', thisTarget.value);
+    });
+
+    inputText.addEventListener('paste', (event) => {
+        const thisTarget = event.target;
+        console.log('paste event. (%s)', thisTarget.value);
+    });
+})();
